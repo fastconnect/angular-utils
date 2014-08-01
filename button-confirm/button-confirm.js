5fe1ddb56441bd52d9096457a81b14ed21f68305
Cédric Mourouvin <cedric.mourouvin@fastconnect.fr>
2014-08-01 15:50:34 +0200

    First working version : 0.0.1_a

--- button-confirm/button-confirm.js
+++ button-confirm/button-confirm.js
@@ -1,36 +1,26 @@
 'use strict';
 
-function ConfirmPopupDirective($templateCache) {
-  return {
-    restrict: 'A',
-    replace: true,
-    scope: {
-      title: '@',
-      content: '@',
-      placement: '@',
-      animation: '&',
-      isOpen: '&',
-    },
-    template: $templateCache.get('button_confirm.html');
-  };
-}
-
-// function confirmDirective(Someservice) {
-//   var tt = $tooltip('confirm', 'confirm', 'click');
-//   tt.controller = 'ConfirmCtrl';
-//   return tt;
-// }
-
 angular.module('angular-utils-ui', ['ui.bootstrap'])
-  .directive('confirmPopup', ['$templateCache', ConfirmPopupDirective($templateCache)])
-  .directive('confirm', ['$tooltip',
+  .directive('confirmPopup', function confirmPopupDirective() {
+    return {
+      restrict: 'A',
+      replace: true,
+      scope: {
+        title: '@',
+        content: '@',
+        placement: '@',
+        animation: '&',
+        isOpen: '&'
+      },
+      template: htmlTooltipConfirm
+    };
+  }).directive('confirm', ['$tooltip',
     function($tooltip) {
       var tt = $tooltip('confirm', 'confirm', 'click');
       tt.controller = 'ConfirmCtrl';
       return tt;
     }
-  ]).controller('ConfirmCtrl', ['$scope', '$attrs', '$parse', ''
-
+  ]).controller('ConfirmCtrl', ['$scope', '$attrs', '$parse',
     function($scope, $attrs, $parse) {
 
       var fn = $parse($attrs.confirmHandler);
@@ -46,4 +36,22 @@ angular.module('angular-utils-ui', ['ui.bootstrap'])
       };
     }
   ]);
-// .directive('confirm', confirmDirective);
+
+/* HTML tooltip */
+var htmlTooltipConfirm = '<div class="popover {{placement}}" ng-class="{ in: isOpen(), fade: animation() }">' +
+  '<div class="arrow"></div>' +
+  '<div class="popover-inner">' +
+  '<h3 class="popover-title" ng-bind="title" ng-show="title"></h3>' +
+  '<div class="popover-content" ng-bind="content"></div>' +
+  '<div class="popover-content">' +
+  '<div class="row" style="width: 250px;">' +
+  '<div class="col-xs-6">' +
+  '<button class="btn-block btn btn-danger" ng-click="$parent.confirm()">Confirm</button>' +
+  '</div>' +
+  '<div class="col-xs-6">' +
+  '<button class="btn btn-default" ng-click="$parent.cancel()">Confirm</button>' +
+  '</div>' +
+  '</div>' +
+  '</div>' +
+  '</div>' +
+  '</div>';
