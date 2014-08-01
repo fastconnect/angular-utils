c2ed9defd603b4fdb231f23850111a71d8ca54fa
Cédric Mourouvin <cedric.mourouvin@fastconnect.fr>
2014-08-01 14:58:44 +0200

    Fix

--- button-confirm/button-confirm.js
+++ button-confirm/button-confirm.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function ConfirmPopupDirective(SomeService) {
+function ConfirmPopupDirective() {
   return {
     restrict: 'A',
     replace: true,
@@ -28,6 +28,21 @@ angular.module('angular-utils-ui', ['ui.bootstrap'])
       var tt = $tooltip('confirm', 'confirm', 'click');
       tt.controller = 'ConfirmCtrl';
       return tt;
+    }
+  ]).controller('ConfirmCtrl', ['$scope', '$attrs', '$parse',
+    function($scope, $attrs, $parse) {
+
+      var fn = $parse($attrs.confirmHandler);
+
+      $scope.cancel = function() {
+        $scope.tt_isOpen = false;
+      };
+
+      $scope.confirm = function() {
+        console.log($attrs.confirmHandler)
+        fn($scope);
+        $scope.tt_isOpen = false;
+      };
     }
   ]);
 // .directive('confirm', confirmDirective);
